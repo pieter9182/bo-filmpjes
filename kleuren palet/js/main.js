e2eef107b7ca7f5bd7166217b7b5e58801ffde94
damian <34666@ma-web.nl>
2024-04-10 22:11:01 +0200

    tot les 5 color picker af

--- kleuren palet/js/main.js
+++ kleuren palet/js/main.js
@@ -2,24 +2,44 @@ class colorCard{
     id;
     color;
     addToList;
+    htmlElement;
+    circle;
+    text;
 
     constructor(newId, newColor, addToList){
+        /*setting properties */
         this.id = newId;
         this.color = newColor;
         this.addToList = addToList;
+
+        /*make lhtmlElement to render */
+        this.htmlElement = document.createElement("li");
+        this.htmlElement.classList = "colors__color";
+    
+        this.circle = document.createElement("figure");
+        this.circle.classList = "colors__circle";
+        this.circle.style.background = this.color;
+
+        this.text = document.createElement("p");
+        this.text.innerText = "copied";
+        this.text.classList = "colors__text";
+        
+        this.htmlElement.onclick = this.onHTMLElementClicked;
+        /*finnaly render */
+        this.render();
     }
 
-    render(){
-        const listItemToBeRendered = document.createElement("li");
-        listItemToBeRendered.classList = "colors__color";
-        const figureToBeRendered = document.createElement("figure");
-        figureToBeRendered.classList = "colors__circle";
-        figureToBeRendered.style.background = this.color;
-        listItemToBeRendered.appendChild(figureToBeRendered);
-        this.addToList.appendChild(listItemToBeRendered);
+    onHTMLElementClicked = () =>{
+        this.circle.classList.add("colors__circle--selected");
+    }
+
+    render(){  
+        this.addToList.appendChild(this.htmlElement);
+        this.htmlElement.appendChild(this.circle);
+        this.htmlElement.appendChild(this.text);
     }
 }
 
-const test = new colorCard(101, "rgba(255,0,0,1)",document.getElementById("js--colors"));
-test.render();
+const test = new colorCard(101, "hsl(284,52%,36%)",document.getElementById("js--colors"));
+
 
